e48b66fbb7efc8e236d5b26b2891d2f409c44b06
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-29 05:36:17 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,17 +1,3 @@
-// Initialize Map Variable
-var map = L.map("map", {
-    center: [],
-    zoom: 2
-  });
-
-// Initialize Mapbox Map Tile Layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  }).addTo(map);
-
 
 // Link for USGS Dataset: Significant Earthquakes in the Past Month
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
@@ -19,7 +5,7 @@ var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significan
 // Retrieve Data
 d3.json(link, function(data) {
 
-    createFeatures(data.features);
+    createMap(data.features);
 
 });
 
