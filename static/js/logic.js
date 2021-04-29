039009a5ecee6fcc924f66d05f01ed02483ff8d8
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-29 05:26:43 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,10 @@
-// Initialize Map
+// Initialize Map Variable
 var map = L.map("map", {
     center: [],
     zoom: 2
   });
 
-
+// Initialize Mapbox Map Tile Layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
