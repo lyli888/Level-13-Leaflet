73377107c0a61caeecc82af909b80b4d5fa7c098
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-29 05:31:20 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -16,10 +16,30 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // Link for USGS Dataset: Significant Earthquakes in the Past Month
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
+// Retrieve Data
 d3.json(link, function(data) {
 
-
+    createFeatures(data.features);
 
 });
 
+function createFeatures(earthquakeData) {
+
+    var earthquakes = L.geoJSON(earthquakeData, {
+    
+   onEachFeature : function (feature, layer) {
+  
+      layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " +  feature.properties.mag + "</p>")
+      },     pointToLayer: function (feature, latlng) {
+        return new L.circle(latlng,
+          {radius: markerSize(feature.properties.mag),
+          fillColor: markerColor(feature.properties.mag),
+          fillOpacity: 1,
+          stroke: false,
+      })
+    }
+    });
+      
+// Add Map Legend
 legend.addto(Map);
