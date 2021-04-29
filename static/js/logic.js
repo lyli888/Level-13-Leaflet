1578080bd20e7b81bd3bd13e66deabab94ff1428
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-29 05:21:47 -0400

    Update logic.js

---
+++ static/js/logic.js
@@ -0,0 +1,15 @@
+// Initialize Map
+var map = L.map("map", {
+    center: [],
+    zoom: 2
+  });
+
+
+// Link for USGS Dataset: Significant Earthquakes in the Past Month
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+d3.json(link, function(data) {
+
+
+    
+});
