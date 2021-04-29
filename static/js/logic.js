1662bd29a2376be94856536b26a0688ca7cc6e14
LL888 <76023384+lyli888@users.noreply.github.com>
2021-04-29 05:24:29 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,6 +5,14 @@ var map = L.map("map", {
   });
 
 
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(map);
+
+
 // Link for USGS Dataset: Significant Earthquakes in the Past Month
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
