4adaddef00c577ad5e84cd02f080b2990cf20562
LL888 <76023384+lyli888@users.noreply.github.com>
2021-05-01 15:44:56 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -44,13 +44,13 @@ d3.json(geolink).then(function(response){
     	});
 
 	//Regular Map
-	var sateliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-		tileSize: 500,
-		maxZoom: 18,
-		zoomOffset: -1,
-		id: "mapbox.satellite",
-		accessToken: API_KEY
+	L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  		attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  		tileSize: 512,
+  		maxZoom: 18,
+  		zoomOffset: -1,
+  		id: "mapbox/streets-v11",
+  		accessToken: API_KEY
 	});
 
 	//Dark Map
