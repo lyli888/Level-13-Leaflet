d8e631179967d104722f61439148414895995aee
LL888 <76023384+lyli888@users.noreply.github.com>
2021-05-01 22:06:45 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,45 @@
+//Regular Map
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+	attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+	tileSize: 512,
+	maxZoom: 18,
+	zoomOffset: -1,
+	id: "mapbox/streets-v11",
+	accessToken: API_KEY
+});
+
+//Dark Map
+var darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+	maxZoom: 18,
+	id: "mapbox.dark",
+	accessToken: API_KEY
+});
+
+//Regular & Dark Map Combined
+var baseMaps = {
+	"Light Map": lightMap,
+	"Dark Map": darkMap
+};
+
+//Put It All Together Into 1 Map With Layers
+var myMap = L.map("map", {
+  //Philadelphia Coordinates
+  center: [39.9526, 75.1652],
+  zoom: 2,
+  layers: [baseMaps, earthquakes]
+});
+
+//Add Toggle Menu
+L.control.layers(lightMap, darkMap).addTo(map);
+
+
+//Add Legend
+var legend = L.control({position: 'bottomright'});
+legend.addTo(myMap);
+
+
+
 
 // Retrieve Data & Make Circles
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(response){
@@ -40,46 +82,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 	earthquakes.addTo(myMap);
 
 });
-
-	//Regular Map
-	var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  		attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  		tileSize: 512,
-  		maxZoom: 18,
-  		zoomOffset: -1,
-  		id: "mapbox/streets-v11",
-  		accessToken: API_KEY
-	});
-
-	//Dark Map
-	var darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  		maxZoom: 18,
-  		id: "mapbox.dark",
-  		accessToken: API_KEY
-	});
-
-	//Regular & Dark Map Combined
-	var baseMaps = {
-  		"Light Map": lightMap,
-  		"Dark Map": darkMap
-	};
-
-	//Put It All Together Into 1 Map With Layers
-	var myMap = L.map("map", {
-		//Philadelphia Coordinates
-		center: [39.9526, 75.1652],
-		zoom: 2,
-		layers: [baseMaps, earthquakes]
-	});
-
-	//Add Toggle Menu
-	L.control.layers(lightMap, darkMap).addTo(map);
-
-
-	//Add Legend
-	var legend = L.control({position: 'bottomright'});
-	legend.addTo(myMap);
 
 
 //TODO Add PoUp Markers
