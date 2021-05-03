4231c6faa94a003f602aaa9eca6ba5ec490c487f
LL888 <76023384+lyli888@users.noreply.github.com>
2021-05-03 15:19:32 -0400

    f

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 function createMap(earthquakes) {
 
   // Create the tile layer that will be the background of our map
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
@@ -10,7 +10,7 @@ function createMap(earthquakes) {
 
   // Create a baseMaps object to hold the lightmap layer
   var baseMaps = {
-    "Light Map": lightmap
+    "Light Map": lightMap
   };
 
   // Create an overlayMaps object to hold the earthquakes layer
@@ -22,7 +22,7 @@ function createMap(earthquakes) {
   var myMap = L.map("map-id", {
     center: [40.73, -74.0059],
     zoom: 12,
-    layers: [lightmap, earthquakes]
+    layers: [lightMap, earthquakes]
   });
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
