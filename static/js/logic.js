79e41365651c29e3cc48ba0ceac6ac78ece833a3
LL888 <76023384+lyli888@users.noreply.github.com>
2021-05-03 13:34:35 -0400

    f

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,35 +1,35 @@
 function createMap(earthquakes) {
 
-    // Create the tile layer that will be the background of our map
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "light-v10",
-      accessToken: API_KEY
-    });
-  
-    // Create a baseMaps object to hold the lightmap layer
-    var baseMaps = {
-      "Light Map": lightmap
-    };
-  
-    // Create an overlayMaps object to hold the earthquakes layer
-    var overlayMaps = {
-      "Earthquakes": earthquakes
-    };
-  
-    // Create the map object with options
-    var myMap = L.map("map-id", {
-      center: [40.73, -74.0059],
-      zoom: 12,
-      layers: [lightmap, earthquakes]
-    });
-  
-    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-  }
+  // Create the tile layer that will be the background of our map
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+
+  // Create a baseMaps object to hold the lightmap layer
+  var baseMaps = {
+    "Light Map": lightmap
+  };
+
+  // Create an overlayMaps object to hold the bikeStations layer
+  var overlayMaps = {
+    "Earthquakes": earthquakes
+  };
+
+  // Create the map object with options
+  var map = L.map("map-id", {
+    center: [40.73, -74.0059],
+    zoom: 12,
+    layers: [lightmap, earthquakes]
+  });
+
+  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+}
   
   function createMarkers(response) {
   
@@ -47,7 +47,7 @@ function createMap(earthquakes) {
 		  console.log(location);
   
       // For each station, create a marker and bind a popup with the station's name
-      var quakeMarker = L.marker([response.features[i].geometry.coordinates[0],response.features[i].geometry.coordinates[1]])
+      var quakeMarker = L.marker(location)
       .bindPopup("<h3>" + response.features[i].properties.place + "<h3><h3>Magnitude: " + response.features[i].properties.mag + "</h3>");
   
       // Add the marker to the quakeMarkers array
