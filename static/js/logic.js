7b3ffc8256faa5a4686a12f657a1445dc754ec63
LL888 <76023384+lyli888@users.noreply.github.com>
2021-05-03 20:05:15 -0400

    f

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,8 @@
 var geolink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
+// Perform an API call to the USGS API to get earthquake information. Call createMarkers 
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(createMarkers);
+
 function createMap(earthquakes) {
 
   // Create the tile layer that will be the background of our map
@@ -33,8 +36,41 @@ function createMap(earthquakes) {
   }).addTo(myMap);
 
 }
+  
+function createMarkers(response) {
+  
+    //Earthquake BindUp Markers
+    var quakeMarkers = [];
 
-//Circle Marker Radius Function
+    // Pull data from response
+    for (var i = 0; i < response.features.length; i++) {
+      var place = response.features[i].properties.place;
+    	var mag = response.features[i].properties.mag;
+      var date = response.features[i].properties.time;
+    	var location = [response.features[i].geometry.coordinates[0], response.features[i].geometry.coordinates[1]];
+    	var depth = response.features[i].geometry.coordinates[2];
+
+		  console.log(location);
+  
+      // For each station, create a marker and bind a popup with the station's name
+      var quakeMarker = L.marker(location)
+      .bindPopup("<h3>" + response.features[i].properties.place + "<h3><h3>Magnitude: " + response.features[i].properties.mag + "</h3>"+ "<h3><h3>Depth: " + response.features[i].geometry.coordinates[2] + "</h3>"+ "<h3><h3>Date: " + response.features[i].properties.time+ "</h3>");
+  
+      // Add the marker to the quakeMarkers array
+      quakeMarkers.push(quakeMarker);
+    }
+
+    // Create a layer group made from the quake markers array, pass it into the createMap function
+    createMap(L.layerGroup(quakeMarkers));
+    addCircles(quakeMarkers);
+}
+  
+  function addCircles(quakeMarkers){
+
+    
+}
+
+  //Circle Marker Radius Function
 function quakeRadius(mag) {
   return ((mag+1) ** 2) * 50000;
 }
@@ -55,35 +91,3 @@ function quakeColor(depth) {
       return "#000000";
   };
 }
-  
-function createMarkers(response) {
-  
-    //Earthquake BindUp Markers
-    var quakeMarkers = [];
-
-  
-    // Pull data from response
-    for (var i = 0; i < response.features.length; i++) {
-      var place = response.features[i].properties.place;
-    	var mag = response.features[i].properties.mag;
-      var date = response.features[i].properties.time;
-    	var location = [response.features[i].geometry.coordinates[0], response.features[i].geometry.coordinates[1]];
-    	var depth = response.features[i].geometry.coordinates[2];
-
-		  console.log(location);
-  
-      // For each station, create a marker and bind a popup with the station's name
-      var quakeMarker = L.marker(location)
-      .bindPopup("<h3>" + response.features[i].properties.place + "<h3><h3>Magnitude: " + response.features[i].properties.mag + "</h3>"+ "<h3><h3>Depth: " + response.features[i].geometry.coordinates[2] + "</h3>"+ "<h3><h3>Date: " + response.features[i].properties.time+ "</h3>");
-  
-      // Add the marker to the quakeMarkers array
-      quakeMarkers.push(quakeMarker);
-      }
-
-    // Create a layer group made from the quake markers array, pass it into the createMap function
-    createMap(L.layerGroup(quakeMarkers));
-  }
-  
-  // Perform an API call to the USGS API to get earthquake information. Call createMarkers 
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(createMarkers);
-  
